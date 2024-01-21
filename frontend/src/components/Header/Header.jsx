28b7745d5cb87b28260c02b03600aa9bc3ada9e9
JOYJOS <enjoyjos@hotmail.com>
2024-01-21 13:22:21 +0100

    Scaffolding and Header component created

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import './header.css';
+import { Link } from 'react-router-dom';
+
+export const Header = () => {
+  return (
+    <div className='fondoHeader'>
+      <header>
+        <div>
+          <Link to='/' className='trooking'>
+            Trooking
+          </Link>
+        </div>
+        <div>
+          <Link to='/misreservas'>
+            <button className='misReservas'> Mis Reservas </button>
+          </Link>
+        </div>
+      </header>
+    </div>
+  );
+};
